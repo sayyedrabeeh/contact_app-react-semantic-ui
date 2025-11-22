40dff8667bd9d596f38dce9c99ea9cb40f474745
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-22 09:21:17 +0530

    modification for firebase

--- contact_app/src/components/firebaseConfig.js
+++ contact_app/src/components/firebaseConfig.js
@@ -1,15 +1,16 @@
+// src/components/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDEVaUuazdkD2FfHVHROKcMcbdUyqVm-Ko",
+  authDomain: "contact-app-94c8a.firebaseapp.com",
+  projectId: "contact-app-94c8a",
+  storageBucket: "contact-app-94c8a.firebasestorage.app",
+  messagingSenderId: "1026701911994",
+  appId: "1:1026701911994:web:494ed79cbf49bf1b1f1f83",
+  measurementId: "G-G70G5B7WJN"
 };
 
 const app = initializeApp(firebaseConfig);
